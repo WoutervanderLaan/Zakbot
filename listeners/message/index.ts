5dbbb32f806d8b8af6cb8aaae929e985bf68c64c
Wouter van der Laan <wouter_vanderlaan93@hotmail.com>
2025-02-08 22:19:43 +0100

    added DM listener

--- listeners/message/index.ts
+++ listeners/message/index.ts
@@ -1,10 +1,12 @@
 import type { App } from "@slack/bolt";
 import reply from "./reply";
 import tag from "./tag";
+import directMessage from "./direct-message";
 
 const TRIGGERS = ["zak", "zakbot", "ghosts", "ghost", "paranormal"];
 
 const register = (app: App) => {
+  app.message(directMessage);
   app.message(/<@U08BT9MFNMA>/, tag);
   TRIGGERS.forEach((trigger) => app.message(trigger, reply));
 };
